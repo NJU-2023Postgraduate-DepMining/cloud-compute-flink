ef0f16b68ff22784b5a9b03eabecc96c2df7e0f2
Azure <987779072@qq.com>
2023-12-04 17:10:11 +0800

    feat: sink local

--- src/main/java/org/example/npm/NpmPackageDependencyFunction.java
+++ src/main/java/org/example/npm/NpmPackageDependencyFunction.java
@@ -2,17 +2,21 @@ package org.example.npm;
 
 import org.apache.flink.api.common.functions.FlatMapFunction;
 import org.apache.flink.api.java.tuple.Tuple2;
+import org.apache.flink.api.java.tuple.Tuple3;
+import org.apache.flink.api.java.tuple.Tuple4;
 import org.apache.flink.util.Collector;
 import com.fasterxml.jackson.databind.JsonNode;
 
+import java.sql.Timestamp;
 import java.util.Iterator;
 
-public class NpmPackageDependencyFunction implements FlatMapFunction<JsonNode, Tuple2<String, Integer>> {
+public class NpmPackageDependencyFunction implements FlatMapFunction<JsonNode, Tuple4<String,String,Long, Integer>> {
     @Override
-    public void flatMap(JsonNode json, Collector<Tuple2<String, Integer>> out) {
+    public void flatMap(JsonNode json, Collector<Tuple4<String, String,Long, Integer>> out) {
 //        System.out.println("json: " + json);
 
         JsonNode versions = json.get("versions");
+        JsonNode times=json.get("time");
 
 //        System.out.println("versions: " + versions);
 
@@ -21,10 +25,14 @@ public class NpmPackageDependencyFunction implements FlatMapFunction<JsonNode, T
         }
 
         for (JsonNode v : versions) {
+            String nodeVersion=v.get("version").asText();
 //            System.out.println("v: " + v);
-
+            String time=times.get(nodeVersion).asText().replace("T"," ").replace("Z","");
+ //           System.out.println("time: " + time);
             JsonNode dependencies = v.get("dependencies");
-
+            Timestamp ts = Timestamp.valueOf(time);
+            Long timeLong=ts.getTime();
+//            System.out.println("timeLong: " + timeLong);
 //            System.out.println("dependencies: " + dependencies);
 
             if (dependencies == null) {
@@ -32,11 +40,13 @@ public class NpmPackageDependencyFunction implements FlatMapFunction<JsonNode, T
             }
 
             for (Iterator<String> it = dependencies.fieldNames(); it.hasNext(); ) {
-                String key = it.next();
+                String packageName = it.next();
+                String version = dependencies.get(packageName).asText();
 
 //                System.out.println("key: " + key);
 
-                out.collect(new Tuple2<>(key, 1));
+                //out.collect(new Tuple2<>(key, 1));
+                out.collect(new Tuple4<>(packageName,version,timeLong, 1));
             }
         }
 
